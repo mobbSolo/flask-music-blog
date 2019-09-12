f2afb3c6c64035581384ec0e86f16fa68270a568
markie_tee <cassette.head@gmail.com>
2019-09-12 14:48:33 -0600

    Configure route for index() to pull posts from the database
    Remove mock posts, and insert a query to pass to the template.
    Add two bullshit posts

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,20 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
+from app.models import Post
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Markie'}
-    posts = [
-        {
-            'title': 'New Beats!',
-            'body': 'This is a video of me staring through Alex\'s window at night',
-            'link': 'https://www.youtube.com/embed/pZXUo_gf5Do'
-        },
-        {
-            'title': 'Super-Duty-Tough Drums',
-            'body': 'You can\'t fake this kinda punch, kadd',
-            'link': 'https://www.youtube.com/embed/GVJYvwI1500'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    # thang = Post.query.first()
+    posts = Post.query.order_by(Post.timestamp).all()
+        # {
+            # 'title': 'New Beats!',
+            # 'body': 'This is a video of me staring through Alex\'s window at night',
+            # 'link': 'https://www.youtube.com/embed/pZXUo_gf5Do'
+        # },
+        # {
+            # 'title': 'Super-Duty-Tough Drums',
+            # 'body': 'You can\'t fake this kinda punch, kadd',
+            # 'link': 'https://www.youtube.com/embed/GVJYvwI1500'
+        # }
+    return render_template('index.html', title='Home', posts=posts )
