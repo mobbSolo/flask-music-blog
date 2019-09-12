1bd8d399bc1d41a7db0aff367497230c0267d938
markie_tee <cassette.head@gmail.com>
2019-09-12 15:28:06 -0600

    Implement pagination to index page
    Also sub-template _posts.html

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,16 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from app import app
 from app.models import Post
 
 @app.route('/')
 @app.route('/index')
 def index():
-    # thang = Post.query.first()
-    posts = Post.query.order_by(Post.timestamp).all()
-        # {
-            # 'title': 'New Beats!',
-            # 'body': 'This is a video of me staring through Alex\'s window at night',
-            # 'link': 'https://www.youtube.com/embed/pZXUo_gf5Do'
-        # },
-        # {
-            # 'title': 'Super-Duty-Tough Drums',
-            # 'body': 'You can\'t fake this kinda punch, kadd',
-            # 'link': 'https://www.youtube.com/embed/GVJYvwI1500'
-        # }
-    return render_template('index.html', title='Home', posts=posts )
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp).paginate(
+            page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) \
+            if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+            if posts.has_prev else None
+    return render_template('index.html', title='Home', posts=posts.items,
+                            next_url=next_url, prev_url=prev_url)
