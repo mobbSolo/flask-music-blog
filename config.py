9d7ea5c8d974a835c3881a077dc42f47d5e20c50
markie_tee <cassette.head@gmail.com>
2019-09-16 14:15:01 -0600

    Various formatting tweeks for homepage

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['cassette.head@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 4
