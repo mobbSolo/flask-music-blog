8f622258901d1262f8f31a2270d216eae2e0a9f3
markie_tee <cassette.head@gmail.com>
2019-12-24 00:59:11 -0700

    Initialize Flask-Admin for our app

--- app/__init__.py
+++ app/__init__.py
@@ -5,47 +5,48 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from flask_admin import Admin
-#from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+app_name = app.config['APP_NAME']
 
-#from app import routes, models, errors
-#admin = Admin(app)
-#admin.add_view(ModelView(models.Post, db.session))
-# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+from app import routes, models, errors
+admin = Admin(app, name=f'{app_name} Admin Panel')
+admin.add_view(ModelView(models.Post, db.session))
+app.config['FLASK_ADMIN_SWATCH'] = 'solar'
 
 # ERROR LOGGING TO EMAIL
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Musicblog Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+# if not app.debug:
+    # if app.config['MAIL_SERVER']:
+        # auth = None
+        # if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            # auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        # secure = None
+        # if app.config['MAIL_USE_TLS']:
+            # secure = ()
+        # mail_handler = SMTPHandler(
+            # mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            # fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            # toaddrs=app.config['ADMINS'], subject='Musicblog Failure',
+            # credentials=auth, secure=secure)
+        # mail_handler.setLevel(logging.ERROR)
+        # app.logger.addHandler(mail_handler)
 
-    # ERROR Logging to file
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/musicblog.log', maxBytes=10240,
-                                        backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+# ERROR Logging to file
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+file_handler = RotatingFileHandler('logs/musicblog.log', maxBytes=10240,
+                                    backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Musicblog startup')
+app.logger.setLevel(logging.INFO)
+app.logger.info('Musicblog startup')
 
